ce6be0ae94990c7caaa9a1c080b3985a9a842383
anwerghanmi <ghanmianwer1@gmail.com>
2022-10-24 22:08:26 +0100

    for loop

---
+++ Python-Flask/Week1/Day1/practice-assignment/for_loop1/ForLoop.py
@@ -0,0 +1,23 @@
+# for x in range(0,151):
+#     print(x)
+
+# for i in range(0,5000,5):
+#     print(i)
+# for x in range(1, 101):
+#     if  x % 10 == 0:
+#         print("coding dojo")
+#     elif x % 5 == 0:
+#         print("coding ")
+#     else:
+#         print(x)
+#         sum=0
+# for x in range (0,500001): 
+#     if x % 2==0:
+#         sum=sum+x
+# print(sum)
+sum=0
+for x in range(1,11,2):
+    print(x)
+else:
+    sum=sum + x
+    print(sum)
