0b59e5b6b45e4f549711f99ffb1eff182155d981
anwerghanmi <ghanmianwer1@gmail.com>
2022-11-01 13:10:04 +0100

    first commit

---
+++ Python-Flask/Week1/Day1/core-assignment/nested_dictionnairie/python.py
@@ -0,0 +1,67 @@
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# x[1][0]=15
+# print(x)
+
+# students[0]["last_name"]="bryant"
+# print(students)
+
+# sports_directory["soccer"][0]="andres" 
+# print(sports_directory)
+
+# z[0]["y"]=30
+# print(z)
+
+# students = [
+#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#          {'first_name' : 'John', 'last_name' : 'Rosales'},
+#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+
+# def iterate_dictionary(list):
+#     for i in range(0, len(list)):
+#         output = ""
+#         for key,val in list[i].items():
+#             output += f" {key} - {val},"
+#         print(output)
+
+# iterate_dictionary(students)
+
+# def iterateDictionary2(key_name, some_list):
+#     for i in range(0, len(some_list)):
+#         for key,val in some_list[i].items():
+#             if key==key_name:
+#                 print(val)
+
+# iterateDictionary2('first_name',students)
+
+
+# def iterateDictionary2(key_name, some_list):
+#     for i in range(0, len(some_list)):
+#         for key,val in some_list[i].items():
+#             if key==key_name:
+#                 print(val)
+
+# iterateDictionary2('last_name',students)
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def print_info(some_dict):
+    for key,val in some_dict.items():
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+
+print_info(dojo)  
