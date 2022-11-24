0223acc651b4748e55a9bab1a488a53c75d2c26e
anwerghanmi <ghanmianwer1@gmail.com>
2022-11-24 11:56:28 +0100

    First React With JSX assignment

---
+++ MERN/Week1/Day3/core-assignment/first_jsx/first-jsx/src/App.js
@@ -0,0 +1,23 @@
+
+
+import './App.css';
+import AnotherComponent from './components/AnotherComponent';
+
+function App() {
+
+  
+  
+
+
+
+  return (
+   
+      <div className="App">
+      
+      
+       <AnotherComponent/>
+      </div>
+    
+  );
+}
+export default App;
---
+++ MERN/Week1/Day3/core-assignment/first_jsx/first-jsx/src/components/AnotherComponent.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+class AnotherComponent extends Component {
+    
+    render() {
+      return(
+        <fieldset>
+
+            <h1>hello world</h1>
+            <h3>things i need to do :</h3>
+            <li>
+          learn react
+        </li>
+        <li>
+    Climb Mt Everest
+        </li>
+        <li>
+          Run a marathon
+        </li>
+        <li>
+          Feed the dogs
+        </li>
+        </fieldset>
+      )
+    }
+}
+export default AnotherComponent;
