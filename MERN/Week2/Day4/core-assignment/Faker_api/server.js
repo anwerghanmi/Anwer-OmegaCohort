6931c786ab09d9a42e12cc053ecd905108ad630e
anwerghanmi <ghanmianwer1@gmail.com>
2022-12-04 19:15:13 +0100

    first commit

--- MERN/Week2/Day4/core-assignment/Faker_api/server.js
+++ MERN/Week2/Day4/core-assignment/Faker_api/server.js
@@ -1,41 +1,46 @@
+const  { faker } = require ('@faker-js/faker') ;
 const express = require("express");
-const faker = require('@faker-js/faker');
 const app = express();
+const PORT =8000;
+
+
+app.get('/api/user/new',(request,responce)=>{
+    responce.send(newuser)
+});
+
+app.get('/api/company/new',(request,responce)=>{
+    responce.send(company)
+});
+
+app.get('/api/user/company',(request,responce)=>{
+    responce.json({company,newuser})
+});
+
+const server= app.listen(PORT,()=>{console.log(`this is server running `);})
 
 class User {
-    constructor() {
-        this.id = faker.random.number();
-        this.firstName = faker.random.firsName();
-        this.lastName = faker.random.lastName();
-        this.phoneNumber = faker.random.phoneNumber();
-        this.email = faker.random.email();
-        this.passeword = faker.random.passeword();
+    constructor(){
+        this.password=faker.internet.password()
+        this.email=faker.internet.email()
+        this.phoneNumber=faker.phone.number()
+        this.lastName=faker.name.lastName()
+        this.firstName=faker.name.firstName()
+        this.id=faker.datatype.uuid()
+    } 
+};
+class Company{
+    constructor(){
+        this.id=faker.datatype.uuid()
+        this.name=faker.company.name()
+        this.address={
+            street:faker.address.street(),
+            city:faker.address.cityName(),
+            state:faker.address.state(),
+            zipCode:faker.address.zipCode(),
+            country:faker.address.county()
+        }
     }
 }
-const newUser = new User ();
-console.log(newUser);
 
-
-class Company {
-    constructor() {
-        this._id = faker.random.number();
-        this.name= faker.company.companyName();
-        this.address = newAdd;
-    }
-  }
-  const newComp = new Company();
-  console.log(newComp);
-  app.get("/api/:users/:new", (req, res) => {
-    if(req.params.users === "users"){
-      res.send(`User First Name: ${newUser.firstName}; User Last Name: ${newUser.lastName}`);
-    }
-    if(req.params.users === "companies"){
-        res.send(`Company name: ${newComp.name}`);
-      }
-      if(req.params.users === "user" && req.params.new === "company"){
-        res.send(`User First Name: ${newUser.firstName}; Company name: ${newComp.name}`);
-      }
-    });
-    const server = app.listen(8000, () =>
-      console.log(`Server is locked and loaded on port ${server.address().port}!`)
-    );
+const newuser = new User();
+const company=new Company();
